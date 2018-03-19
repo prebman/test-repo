3e1640020d2333b0b0358e515b9209620d2d9c38
Paul Rebman <prebman14@gmail.com>
2018-03-20 01:44:07 +0300

    Update formbuildertest.js

--- formbuildertest.js
+++ formbuildertest.js
@@ -1,4 +1,9 @@
+// Get the button
+var btn = document.getElementById("id123-button-send");
 
+// Save and remove the onclick handler
+var clickHandler = btn[0].onclick;
+btn[0].onclick = false;
 
 document.getElementById("id123-button-send").onclick = function () {
 var gps = document.getElementById("id123-control37592984").value;
