4a22711cf99647dbd7a10f19e8061a14339cf375
Paul Rebman <prebman14@gmail.com>
2018-03-20 01:03:38 +0300

    Update formbuildertest.js
    Added semicolons after lines in functions

--- formbuildertest.js
+++ formbuildertest.js
@@ -1,8 +1,8 @@
 document.getElementById("id123-control37592971").onfocus = function() {
-document.getElementById("id123-control37592971").style.backgroundColor = "red"
+document.getElementById("id123-control37592971").style.backgroundColor = "red";
 }
 
 
 document.getElementById("id123-control37592971").onblur = function() {
-document.getElementById("id123-control37592971").style.backgroundColor = "blue"
+document.getElementById("id123-control37592971").style.backgroundColor = "blue";
 }
