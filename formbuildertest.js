2afd1b33006e32a1b71b74ea7c850bb3a18da383
Paul Rebman <prebman14@gmail.com>
2018-03-20 10:01:00 +0300

    Update formbuildertest.js
    Deleted everything and am tyring out setting a field to readonly

--- formbuildertest.js
+++ formbuildertest.js
@@ -1,11 +1,1 @@
-// Get the button
-var btn = document.getElementById("id123-button-send");
-
-// Save and remove the onclick handler
-var clickHandler = btn[0].onclick;
-btn[0].onclick = false;
-
-document.getElementById("id123-button-send").onclick = function () {
-var gps = document.getElementById("id123-control37592984").value;
-alert(gps);
-}
+document.getElementByID.("id123-control38012838").readOnly = True;
